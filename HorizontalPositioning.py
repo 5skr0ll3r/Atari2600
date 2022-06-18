7615303c20483625c86b34179c8c94f78a27fd0c
5skr0ll3r <47701859+5skr0ll3r@users.noreply.github.com>
2022-06-18 17:28:08 +0300

    Update HorizontalPositioning.py

--- HorizontalPositioning.py
+++ HorizontalPositioning.py
@@ -10,26 +10,31 @@ def realPosX(currentXPos, HBlank):
 	positionX = currentXPos + HBlank 
 	return positionX
 
-def setPositionX(realPosX, tiaCycles, fineNum, HDirection, asl):
+def setPositionX(positionX, tiaCycles, fineNum, HDirection, asl):
 	remainder = positionX % tiaCycles
 	xor = remainder ^ tiaCycles
 	AShiftLeft = xor << asl
 	print('=' * 20)
-	print(f'Normal\nCurrent X Position (with HBlank as offset): {realPosX} \nMoving {HDirection}\nRemainder (sbc): {remainder}\n(xor): {xor}\n(asl): {AShiftLeft}\n')
-	print(f'Bin\nCurrent X Position (with HBlank as offset): {bin(realPosX)} \nMoving {HDirection}\nRemainder (sbc): {bin(remainder)}\n(xor): {bin(xor)}\n(asl): {bin(AShiftLeft)}\n')
-
-positionX = realPosX(currentXPos, HBlank)
+	print(f'Normal\nCurrent X Position (with HBlank as offset): {positionX} \nMoving {HDirection}\nRemainder (sbc): {remainder}\n(xor): {xor}\n(asl): {AShiftLeft}\n')
+	print(f'Bin\nCurrent X Position (with HBlank as offset): {bin(positionX)} \nMoving {HDirection}\nRemainder (sbc): {bin(remainder)}\n(xor): {bin(xor)}\n(asl): {bin(AShiftLeft)}\n')
 
 
+def main():
+	positionX = realPosX(currentXPos, HBlank)
 
-while True:
-	key = input("Press key: ")
-	if(key == 'a'):
-		HDirection = "Left"
-		positionX -= 1
 
-	elif(key == 'd'):
-		HDirection = "Right"
-		positionX += 1
 
-	setPositionX(positionX, 15, calcTIACycles(cpuCycles), HDirection, 4)
+	while True:
+		key = input("Press key: ")
+		if(key == 'a'):
+			HDirection = "Left"
+			positionX -= 1
+
+		elif(key == 'd'):
+			HDirection = "Right"
+			positionX += 1
+
+		setPositionX(positionX, 15, calcTIACycles(cpuCycles), HDirection, 4)
+
+if __name__ == '__main__':
+	main()
