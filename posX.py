9b402d68e44ba5274938366293d999efbd1c6c4c
5skr0ll3r <47701859+5skr0ll3r@users.noreply.github.com>
2022-06-18 17:22:00 +0300

    Add files via upload

---
+++ posX.py
@@ -0,0 +1,35 @@
+currentXPos = 15
+HDirection = None
+HBlank = 68
+cpuCycles = 5
+
+def calcTIACycles(cpuCycles):
+	return cpuCycles * 3
+
+def realPosX(currentXPos, HBlank):
+	positionX = currentXPos + HBlank 
+	return positionX
+
+def setPositionX(realPosX, tiaCycles, fineNum, HDirection, asl):
+	remainder = positionX % tiaCycles
+	xor = remainder ^ tiaCycles
+	AShiftLeft = xor << asl
+	print('=' * 20)
+	print(f'Normal\nCurrent X Position (with HBlank as offset): {realPosX} \nMoving {HDirection}\nRemainder (sbc): {remainder}\n(xor): {xor}\n(asl): {AShiftLeft}\n')
+	print(f'Bin\nCurrent X Position (with HBlank as offset): {bin(realPosX)} \nMoving {HDirection}\nRemainder (sbc): {bin(remainder)}\n(xor): {bin(xor)}\n(asl): {bin(AShiftLeft)}\n')
+
+positionX = realPosX(currentXPos, HBlank)
+
+
+
+while True:
+	key = input("Press key: ")
+	if(key == 'a'):
+		HDirection = "Left"
+		positionX -= 1
+
+	elif(key == 'd'):
+		HDirection = "Right"
+		positionX += 1
+
+	setPositionX(positionX, 15, calcTIACycles(cpuCycles), HDirection, 4)
