e3fd4992c6552288f08b061c4b75287cf330c852
5skr0ll3r <47701859+5skr0ll3r@users.noreply.github.com>
2022-06-19 14:36:31 +0300

    Update HorizontalPositioning.py

--- HorizontalPositioning.py
+++ HorizontalPositioning.py
@@ -10,13 +10,13 @@ def realPosX(currentXPos, HBlank):
 	positionX = currentXPos + HBlank 
 	return positionX
 
-def setPositionX(positionX, tiaCycles, fineNum, HDirection, asl):
+def setPositionX(positionX, tiaCycles, HDirection, asl):
 	remainder = positionX % tiaCycles
 	xor = remainder ^ tiaCycles
 	AShiftLeft = xor << asl
 	print('=' * 20)
-	print(f'Normal\nCurrent X Position (with HBlank as offset:68): {positionX} \nMoving {HDirection}\nRemainder (sbc): {remainder}\n(xor): {xor}\n(asl): {AShiftLeft}\n')
-	print(f'Bin\nCurrent X Position (with HBlank as offset:68): {bin(positionX)} \nMoving {HDirection}\nRemainder (sbc): {bin(remainder)}\n(xor): {bin(xor)}\n(asl): {bin(AShiftLeft)}\n')
+	print(f'Normal\nCurrent X Position (with HBlank as offset): {positionX} \nMoving {HDirection}\nRemainder (sbc): {remainder}\n(xor): {xor}\n(asl): {AShiftLeft}\n')
+	print(f'Bin\nCurrent X Position (with HBlank as offset): {bin(positionX)} \nMoving {HDirection}\nRemainder (sbc): {bin(remainder)}\n(xor): {bin(xor)}\n(asl): {bin(AShiftLeft)}\n')
 
 
 def main():
@@ -34,7 +34,7 @@ def main():
 			HDirection = "Right"
 			positionX += 1
 
-		setPositionX(positionX, 15, calcTIACycles(cpuCycles), HDirection, 4)
+		setPositionX(positionX, calcTIACycles(cpuCycles), HDirection, 4)
 
 if __name__ == '__main__':
 	main()
